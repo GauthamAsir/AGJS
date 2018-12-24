2cbdccb03b6461f0c45280dcc5ab80784d3f5019
Gautham <gauthamasir@gmail.com>
2018-12-25 00:58:18 +0530

    Add Database Helper
    Signed-off-by: Gautham <gauthamasir@gmail.com>

---
+++ app/src/main/java/gautham/agjs/institute/UpdateData.java
@@ -0,0 +1,20 @@
+package gautham.agjs.institute;
+
+public class UpdateData {
+
+    private String Name;
+    private String Time;
+
+    public UpdateData(String name, String time) {
+        Name = name;
+        Time = time;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+}
